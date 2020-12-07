61fa946e47819c5568eedf72211fb4485b007bec
unknown <welan.c.13+gh@gmail.com>
2020-12-07 14:45:16 +1100

    updated code to PyPoll main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -35,22 +35,28 @@ with open(election_csv, "r") as election_file:
         if row[2] == "O'Tooley":
             otooley_votes[str(row[0])] = int(1)
     
-    khan_total = (sum(khan_votes.values()) / votes_total) * 100
-    correy_total = (sum(correy_votes.values()) / votes_total) * 100
-    li_total = (sum(li_votes.values()) / votes_total) * 100
-    otooley_total = (sum(otooley_votes.values()) / votes_total) * 100
+    khan_percent = (sum(khan_votes.values()) / votes_total) * 100
+    correy_percent = (sum(correy_votes.values()) / votes_total) * 100
+    li_percent = (sum(li_votes.values()) / votes_total) * 100
+    otooley_percent = (sum(otooley_votes.values()) / votes_total) * 100
 
     print(f"Election Results")
     print(f"-------------------------")
     print(f"Total Votes: {votes_total}")
     print(f"-------------------------")
-    print(f"Khan: {round(khan_total,3)}% ({sum(khan_votes.values())})")
-    print(f"Correy: {round(correy_total,3)}% ({sum(correy_votes.values())})")
-    print(f"Li: {round(li_total,3)}% ({sum(li_votes.values())})")
-    print(f"O'Tooley: {round(otooley_total,3)}% ({sum(otooley_votes.values())})")
+    print(f"Khan: {round(khan_percent,3)}% ({sum(khan_votes.values())})")
+    print(f"Correy: {round(correy_percent,3)}% ({sum(correy_votes.values())})")
+    print(f"Li: {round(li_percent,3)}% ({sum(li_votes.values())})")
+    print(f"O'Tooley: {round(otooley_percent,3)}% ({sum(otooley_votes.values())})")
     print(f"-------------------------")
     print(f"Winner: ")
     print(f"-------------------------")
+    
 
+# summate each candidate's dictionary and append that value to a list 
+# add each candidate's name to a separate list 
+# combine both lists into a dictionary: summated votes as the key, candidate names as the values 
+# set a variable equal to the max value of the summated votes
+# use dictionary to find the value associated to the max summated votes key 
 
 
